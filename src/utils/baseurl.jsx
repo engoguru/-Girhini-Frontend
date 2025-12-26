36deaa743206a3ef38fa270a3726aad0cf62d144
engoguru <engoguru@carkverma.com>
2025-12-26 14:57:54 +0530

    new update122222

--- src/utils/baseurl.jsx
+++ src/utils/baseurl.jsx
@@ -1,6 +1,6 @@
 // utils/baseurl.js
 
-const baseUrl = "http://localhost:4500";
-// const baseUrl="https://girhini-backend-1.onrender.com"
+// const baseUrl = "http://localhost:4500";
+const baseUrl="https://girhini-backend-1.onrender.com"
 export default baseUrl;
 // https://girhini-frontend.onrender.com/
